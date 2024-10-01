10843997dc06abc4f9ac4e37ebccb038d5df2fb0
JulieB <bourgeaisjulie@gmail.com>
2024-10-01 15:25:20 +0200

    Debut User

--- main.java
+++ main.java
@@ -1,1 +1,17 @@
 //main 
+/*
+ * Créer des gens
+ * "Vuez vus vus inscrire ?"
+ * ui
+ * nn
+ * 
+ * SI ui
+ * entrz nm, prénm, ...
+ * 
+ * Vus etes qui ?
+ * 
+ * ENtrez de tee u tee manière 
+ * 
+ * n créé e mec
+ * 
+ */
