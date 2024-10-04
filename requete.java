5b92c1ae01381249c154fb218467a749dd53e33d
MlleIlona <ilona.gerard@orange.fr>
2024-10-04 14:32:57 +0200

    Update avis

--- requete.java
+++ requete.java
@@ -4,29 +4,27 @@ public class requete {
 
     protected String nom;
     protected String desc;
+    protected String status;
+    protected Date date;
 
     public requete(String nom, String description) {
         this.nom = nom;
         this.desc = description;
-        Date date = new Date();
-        String status = "en attente";
+        this.date = new Date();
+        this.status = "en attente";
 
     }
 
     // deuxième constructeur car la description est optionnelle
     public requete(String nom) {
         this.nom = nom;
-        Date date = new Date();
-        String status = "en attente";
+        this.date = new Date();
+        this.status = "en attente";
 
     }
 
     public requete getRequete() {
         return this;
-    }
-
-    public void setRequete() {
-        // TO DO
     }
 
 }
